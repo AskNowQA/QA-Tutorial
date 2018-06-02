1cbf412c877027fe67b639fe0c30960647a19f89
lukovnikov <lukovnikov@outlook.com>
2018-06-03 00:11:06 +0200

    added sh

---
+++ session4/pkgs/install_deps.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+conda install blas cffi intel-openmp libgfortran-ng libopenblas ninja pycparser
+conda install mkl-2018.0.2-1.tar.bz2
+conda install mkl_fft-1.0.1-py27h3010b51_0.tar.bz2
+conda install mkl_random-1.0.1-py27h629b387_0.tar.bz2
+conda install numpy-base-1.14.3-py27h2b20989_2.tar.bz2
+conda install numpy-1.14.3-py27hcd700cb_2.tar.bz2
+conda install pytorch-cpu-0.4.0-py27_cpu_1.tar.bz2
